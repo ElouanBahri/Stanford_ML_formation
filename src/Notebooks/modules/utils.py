d5fc0136fc4a5c6d01e3f6b3999003f17ad240d1
elouan_bahri <e.bahri@ggeedu.fr>
2024-09-26 17:09:11 +0200

    format

--- src/Notebooks/modules/utils.py
+++ src/Notebooks/modules/utils.py
@@ -1,13 +1,21 @@
 import numpy as np
 
+
 def load_data():
-    data = np.loadtxt("/Users/elouan/Repo Github ElouanBahri/Stanford_ML_formation/src/Notebooks/modules/data2/ex1data1.txt", delimiter=',')
-    X = data[:,0]
-    y = data[:,1]
+    data = np.loadtxt(
+        "/Users/elouan/Repo Github ElouanBahri/Stanford_ML_formation/src/Notebooks/modules/data2/ex1data1.txt",
+        delimiter=",",
+    )
+    X = data[:, 0]
+    y = data[:, 1]
     return X, y
 
+
 def load_data_multi():
-    data = np.loadtxt("/Users/elouan/Repo Github ElouanBahri/Stanford_ML_formation/src/Notebooks/modules/data2/ex1data2.txt", delimiter=',')
-    X = data[:,:2]
-    y = data[:,2]
+    data = np.loadtxt(
+        "/Users/elouan/Repo Github ElouanBahri/Stanford_ML_formation/src/Notebooks/modules/data2/ex1data2.txt",
+        delimiter=",",
+    )
+    X = data[:, :2]
+    y = data[:, 2]
     return X, y
