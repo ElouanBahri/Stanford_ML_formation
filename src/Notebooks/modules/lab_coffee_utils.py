f0f8431a1c41440c69da859c386915bd898e6a6c
elouan_bahri <e.bahri@ggeedu.fr>
2024-10-01 17:24:39 +0200

    First neural network with tensorflow

--- src/Notebooks/modules/lab_coffee_utils.py
+++ src/Notebooks/modules/lab_coffee_utils.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('./deeplearning.mplstyle')
+plt.style.use('../deeplearning.mplstyle')
 import tensorflow as tf
 from tensorflow.keras.activations import sigmoid
 from matplotlib import cm
 import matplotlib.colors as colors
-from lab_utils_common import dlc
+from modules.lab_utils_common4 import dlc
 
 def load_coffee_data():
     """ Creates a coffee roasting data set.
